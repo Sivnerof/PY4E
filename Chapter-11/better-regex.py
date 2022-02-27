dd79a75f27dd9a2fc9cd8875c7801f08023d4d44
Rogue Blue <rogueblueprint@protonmail.com>
2022-02-26 18:03:21 -0700

    Changed file path

--- Chapter-11/better-regex.py
+++ Chapter-11/better-regex.py
@@ -3,4 +3,4 @@
 
 import re
 
-print(sum([int(num) for num in re.findall('[0-9]+', open('../Text-Files/regex_sum_42.txt').read())]))
+print(sum([int(num) for num in re.findall('[0-9]+', open('../Text-Files/regex_sum_1495646.txt').read())]))
