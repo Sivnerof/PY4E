6731bb1e9b21cf82cb91d7b00a4cf74e0e2977fb
Rogue Blue <rogueblueprint@protonmail.com>
2022-02-28 19:54:03 -0700

    Beautiful Soup Scrape

--- Chapter-12/scraping.py
+++ Chapter-12/scraping.py
@@ -46,3 +46,29 @@
     # Enter - http://py4e-data.dr-chuck.net/comments_42.html
     # Count 50
     # Sum 2...
+
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the span tags
+spans = soup('span')
+
+# Create list for storing values from span
+span_sum = list()
+
+for span in spans:
+    # Look at the parts of a tag
+    span_sum.append(int(span.contents[0]))
+
+print(sum(span_sum))
